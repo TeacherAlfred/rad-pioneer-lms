cc1d04ec3b155722faa888cb787dc4fba1deab28
Alfred Chingombe <alfredc@radacademy.co.za>
2026-08-08 15:10:15 +0200

    fix(webhook): updated language from en to en_US in ADMIN ALERT

--- src/app/api/whatsapp-webhook/route.ts
+++ src/app/api/whatsapp-webhook/route.ts
@@ -213,7 +213,7 @@ export async function POST(request: Request) {
                       template: {
                         name: 'admin_lead_alert',
                         language: {
-                          code: 'en' 
+                          code: 'en_US' 
                         },
                         components: [
                           {
